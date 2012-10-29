d35274b04dc1d5654d0888a086651000d5cad4fe
Takamasa Mioki <t.mioki@gmail.com>
2012-10-29 23:34:35 +0900

    add comment

--- 2_2.hs
+++ 2_2.hs
@@ -3,6 +3,8 @@ import Data.List
 descSort :: [Int] -> [Int]
 descSort = sortBy (\x y -> compare y x)
 
+-- How do I use descSort result?
+-- I want to use descSort result to getCoinCount.
 getCoinCount :: [Int] -> Int -> Int
 getCoinCount _ 0 = 0
 getCoinCount [] _ = 0
